d5b4d55f880c1634614460b0e61c5ff5df57c2af
Qu-jq <1587743243@qq.com>
2020-01-13 22:13:25 +0800

    重新增加JDBC代码

---
+++ myJDBC/src/cn/qjq/jdbctest/demo02.java
@@ -0,0 +1,26 @@
+package cn.qjq.jdbctest;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ *测试执行SQL语句，测试SQL注入问题
+ * Created by qjq on 2020/1/13 20:22
+ */
+public class demo02 {
+    public static void main(String[] args) {
+
+        try {
+            //加载驱动类
+            Class.forName("com.mysql.jdbc.Driver");
+            //建立连接（连接对象内部其实包含了Socket对象，是一个远程的连接，实际很耗时，这是Connection对象管理的一个要点）
+            //实际开发中，为提高效率，使用连接池来管理连接
+            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/test","root","root");
+            System.out.println(connection);
+        } catch (ClassNotFoundException | SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
